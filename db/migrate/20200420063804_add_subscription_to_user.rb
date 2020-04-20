429f7cf118b4f623f8c75f3fd18d5a6d479f9b67
nccaster <ncapara@miuandes.clgit config --global user.email ncapara@miuandes.cl>
2020-04-20 04:59:46 -0400

    testing table

--- db/migrate/20200420063804_add_subscription_to_user.rb
+++ db/migrate/20200420063804_add_subscription_to_user.rb
@@ -1,5 +1,5 @@
 class AddSubscriptionToUser < ActiveRecord::Migration[6.0]
   def change
-    add_reference :users, :subscription, null: true, foreign_key: true
+    add_reference :subscriptions, :user, null: true, foreign_key: true
   end
 end
--- db/migrate/20200420063944_add_subscription_to_organization.rb
+++ db/migrate/20200420063944_add_subscription_to_organization.rb
@@ -1,5 +1,5 @@
 class AddSubscriptionToOrganization < ActiveRecord::Migration[6.0]
   def change
-    add_reference :organizations, :subscription, null: true, foreign_key: true
+    add_reference :subscriptions, :organization, null: true, foreign_key: true
   end
 end
