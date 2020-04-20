9858fd8d63e9c58f57aac412bbeb04969d743bfb
nccaster <ncapara@miuandes.clgit config --global user.email ncapara@miuandes.cl>
2020-04-20 05:57:56 -0400

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -17,12 +17,20 @@ ActiveRecord::Schema.define(version: 2020_04_20_093844) do
     t.integer "reply_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.integer "event_id"
+    t.index ["event_id"], name: "index_comments_on_event_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "date_options", force: :cascade do |t|
     t.date "desired_date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "vote_date_id"
+    t.integer "event_id"
+    t.index ["event_id"], name: "index_date_options_on_event_id"
+    t.index ["vote_date_id"], name: "index_date_options_on_vote_date_id"
   end
 
   create_table "event_files", force: :cascade do |t|
@@ -35,7 +43,11 @@ ActiveRecord::Schema.define(version: 2020_04_20_093844) do
   create_table "event_invitations", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.integer "event_id"
     t.integer "vote_date_id"
+    t.index ["event_id"], name: "index_event_invitations_on_event_id"
+    t.index ["user_id"], name: "index_event_invitations_on_user_id"
     t.index ["vote_date_id"], name: "index_event_invitations_on_vote_date_id"
   end
 
@@ -48,29 +60,23 @@ ActiveRecord::Schema.define(version: 2020_04_20_093844) do
     t.boolean "public"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "event_invitation_id"
-    t.integer "report_id"
-    t.integer "comment_id"
+    t.integer "user_id"
     t.integer "event_file_id"
-    t.integer "date_option_id"
-    t.index ["comment_id"], name: "index_events_on_comment_id"
-    t.index ["date_option_id"], name: "index_events_on_date_option_id"
     t.index ["event_file_id"], name: "index_events_on_event_file_id"
-    t.index ["event_invitation_id"], name: "index_events_on_event_invitation_id"
-    t.index ["report_id"], name: "index_events_on_report_id"
+    t.index ["user_id"], name: "index_events_on_user_id"
   end
 
   create_table "inbox_messages", force: :cascade do |t|
     t.string "message"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "mail_box_id"
+    t.index ["mail_box_id"], name: "index_inbox_messages_on_mail_box_id"
   end
 
   create_table "mail_boxes", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "inbox_message_id"
-    t.index ["inbox_message_id"], name: "index_mail_boxes_on_inbox_message_id"
   end
 
   create_table "notifications", force: :cascade do |t|
@@ -93,22 +99,28 @@ ActiveRecord::Schema.define(version: 2020_04_20_093844) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "subscription_id"
     t.integer "organization_file_id"
     t.index ["organization_file_id"], name: "index_organizations_on_organization_file_id"
-    t.index ["subscription_id"], name: "index_organizations_on_subscription_id"
   end
 
   create_table "reports", force: :cascade do |t|
     t.string "motive"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.integer "event_id"
+    t.index ["event_id"], name: "index_reports_on_event_id"
+    t.index ["user_id"], name: "index_reports_on_user_id"
   end
 
   create_table "subscriptions", force: :cascade do |t|
     t.string "rank"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.integer "organization_id"
+    t.index ["organization_id"], name: "index_subscriptions_on_organization_id"
+    t.index ["user_id"], name: "index_subscriptions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -120,42 +132,30 @@ ActiveRecord::Schema.define(version: 2020_04_20_093844) do
     t.string "name"
     t.string "location"
     t.string "bio"
-    t.integer "subscription_id"
-    t.integer "event_id"
-    t.integer "event_invitation_id"
-    t.integer "report_id"
-    t.integer "comment_id"
     t.integer "mail_box_id"
-    t.index ["comment_id"], name: "index_users_on_comment_id"
-    t.index ["event_id"], name: "index_users_on_event_id"
-    t.index ["event_invitation_id"], name: "index_users_on_event_invitation_id"
     t.index ["mail_box_id"], name: "index_users_on_mail_box_id"
-    t.index ["report_id"], name: "index_users_on_report_id"
-    t.index ["subscription_id"], name: "index_users_on_subscription_id"
   end
 
   create_table "vote_dates", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "date_option_id"
-    t.index ["date_option_id"], name: "index_vote_dates_on_date_option_id"
   end
 
+  add_foreign_key "comments", "events"
+  add_foreign_key "comments", "users"
+  add_foreign_key "date_options", "events"
+  add_foreign_key "date_options", "vote_dates"
+  add_foreign_key "event_invitations", "events"
+  add_foreign_key "event_invitations", "users"
   add_foreign_key "event_invitations", "vote_dates"
-  add_foreign_key "events", "comments"
-  add_foreign_key "events", "date_options"
   add_foreign_key "events", "event_files"
-  add_foreign_key "events", "event_invitations"
-  add_foreign_key "events", "reports"
-  add_foreign_key "mail_boxes", "inbox_messages"
+  add_foreign_key "events", "users"
+  add_foreign_key "inbox_messages", "mail_boxes"
   add_foreign_key "notifications", "users"
   add_foreign_key "organizations", "organization_files"
-  add_foreign_key "organizations", "subscriptions"
-  add_foreign_key "users", "comments"
-  add_foreign_key "users", "event_invitations"
-  add_foreign_key "users", "events"
+  add_foreign_key "reports", "events"
+  add_foreign_key "reports", "users"
+  add_foreign_key "subscriptions", "organizations"
+  add_foreign_key "subscriptions", "users"
   add_foreign_key "users", "mail_boxes"
-  add_foreign_key "users", "reports"
-  add_foreign_key "users", "subscriptions"
-  add_foreign_key "vote_dates", "date_options"
 end
