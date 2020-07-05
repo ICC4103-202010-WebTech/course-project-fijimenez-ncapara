9cec752572e84a6bf57d36527c994424a7b45581
FJJI <fjjimenez@miuandes.cl>
2020-07-05 07:51:57 -0400

    cancancan(can) installed and some basic security thingie that we had forgotten in ORG

--- db/schema.rb
+++ db/schema.rb
@@ -122,7 +122,7 @@ ActiveRecord::Schema.define(version: 2020_07_05_073351) do
 
   create_table "organizations", force: :cascade do |t|
     t.string "name"
-    t.datetime "created_at", precision: 6, null: false
+    t.datetime "created_at",  precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
